6dc6a1c9333ad09d67973c5a6794991a785e3057
Pluxxa <95534978+Pluxxa@users.noreply.github.com>
2024-03-22 13:14:03 +0300

    add

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("/img/AK-47.jpg")
+
 running = True
 while running:
     pass
